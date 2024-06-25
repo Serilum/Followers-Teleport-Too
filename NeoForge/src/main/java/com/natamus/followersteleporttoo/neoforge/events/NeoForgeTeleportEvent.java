afbad9efd08b53858e7d83c1223107767ee466bc
Rick South <rick@natamus.com>
2024-06-26 00:02:20 +0200

    (v2.5) (1.21.0 Neo) Updated mod to work with NeoForge version 21.0.31+.

--- NeoForge/src/main/java/com/natamus/followersteleporttoo/neoforge/events/NeoForgeTeleportEvent.java
+++ NeoForge/src/main/java/com/natamus/followersteleporttoo/neoforge/events/NeoForgeTeleportEvent.java
@@ -3,11 +3,11 @@ package com.natamus.followersteleporttoo.neoforge.events;
 import com.natamus.followersteleporttoo.events.TeleportEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.neoforge.event.entity.EntityTeleportEvent;
-import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityTeleportEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 @EventBusSubscriber
 public class NeoForgeTeleportEvent {
@@ -22,7 +22,7 @@ public class NeoForgeTeleportEvent {
 	}
 
 	@SubscribeEvent
-	public static void onFollowerDamage(LivingHurtEvent e) {
+	public static void onFollowerDamage(LivingIncomingDamageEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 
 		if (TeleportEvent.onFollowerDamage(livingEntity.level(), livingEntity, e.getSource(), e.getAmount()) == 0F) {
