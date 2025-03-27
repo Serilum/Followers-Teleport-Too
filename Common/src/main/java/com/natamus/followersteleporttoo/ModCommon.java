2bd5417ecadcded76a6cc8ed1e7d8f799a07778d
Rick South <rick@natamus.com>
2025-03-28 00:49:20 +0100

    Followers Teleport Too source code for 1.21.5.

---
+++ Common/src/main/java/com/natamus/followersteleporttoo/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.followersteleporttoo;
+
+import com.natamus.followersteleporttoo.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
